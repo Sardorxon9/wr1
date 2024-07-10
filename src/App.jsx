cfa8e04071446db0a709961e0f93261f2f019fb4
Sardorkhon Khamidov <sardorkhon09@gmail.com>
2024-07-10 11:36:53 +0300

    folders

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 // App.jsx
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import LoginPage from './components/loginPage';
-import RegistrationOwner from './components/registrationOwner';
-import SuccessPage from './components/successPage';
+import LoginPage from './components/login-signUp/loginPage';
+import RegistrationOwner from './components/login-signUp/registrationOwner';
+import SuccessPage from './components/login-signUp/successPage';
 import './App.css';
 
 export default function App() {
